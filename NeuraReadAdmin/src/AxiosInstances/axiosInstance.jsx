b7e0c39a803e5319f92c14923574c88078d37d35
SHOURYA-SINHA <jmditolima@gmail.com>
2025-03-12 12:30:27 +0700

    update server url

--- NeuraReadAdmin/src/AxiosInstances/axiosInstance.jsx
+++ NeuraReadAdmin/src/AxiosInstances/axiosInstance.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstances = axios.create({
-  // baseURL: 'https://bdo-bank.onrender.com/api/v1/',
+  baseURL: 'https://bdo-bank.onrender.com/api/v1/',
   // baseURL: 'https://10.0.2.2:7272/api/v1/',
-  baseURL: 'http://localhost:7272/api/v1/',
+  // baseURL: 'http://localhost:7272/api/v1/',
 
   withCredentials: true,
   headers: {
